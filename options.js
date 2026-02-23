4240f7f92ed0f225563c965beb90d10276bd8caa
iced-code github <ayaanmodak1@gmail.com>
2026-02-22 20:16:15 -0500

    Updated options screen

--- options.js
+++ options.js
@@ -2,6 +2,8 @@ document.querySelectorAll(".colorButton").forEach(button => {
     button.addEventListener("click", async () => {
         let color = button.value;
         localStorage.setItem("button_color", color);
+
+        document.getElementById("main").classList = [color];
     });
 })
 
@@ -17,6 +19,9 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("toggleMode").innerHTML = "&#128161;";
     }
     
+    let buttonColor = localStorage.getItem("button_color") || "green";
+    document.getElementById("main").classList = [buttonColor];
+
     document.getElementById("toggleMode").addEventListener("click", toggleMode);
 });
 function toggleMode(){
